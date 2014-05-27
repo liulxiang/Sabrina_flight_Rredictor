2f249ff4556732c1128591b20ba6f91f4aae6da2
liulxiang <liulxiang@gmail.com>
2014-05-28 00:17:54 +0800

    src/main/common/com/taotaoti/common/utils/MainCSV.java

--- src/main/common/com/taotaoti/common/utils/MainCSV.java
+++ src/main/common/com/taotaoti/common/utils/MainCSV.java
@@ -1,5 +1,6 @@
 package com.taotaoti.common.utils;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,15 +16,29 @@ public class MainCSV {
 	 * @return
 	 * @throws IOException 
 	 */
-	public List<String[]> readCsv(String fileName) throws IOException{
-		reader = new CSVReader(new FileReader(fileName));  
+	public Double[] readCsv(String fileName) throws IOException{
+		File file=new File(fileName);
+		System.out.println(file.getAbsolutePath());
+		reader = new CSVReader(new FileReader(file));  
 	    String [] nextLine;  
-	    List<String[]> contents=new ArrayList<String[]>();
-	    while ((nextLine = reader.readNext()) != null) {  
-	    	contents.add(nextLine);
+	    List<Double> contents=new ArrayList<Double>();
+	    while ((nextLine = reader.readNext()) != null) { 
+	    	if(nextLine!=null&&nextLine[0]!=null){
+	    		Double temp=Double.valueOf(nextLine[0]);
+	    		contents.add(temp);
+	    	}
 	    }  
 	    reader.close();
-	    return contents;
+	    Double[] cs=new Double[contents.size()]; 
+	    for(int i=0;i<contents.size();i++){
+	    	cs[i]=contents.get(i);
+	    }
+	    return cs;
+	}
+	public static void main(String[] args) throws IOException {
+		MainCSV mainCSV=new MainCSV();
+		Double[] csv=mainCSV.readCsv("FlightNo/108.csv");
+		System.out.println(ObjToStringUtil.objToString(csv));
 	}
 
 
